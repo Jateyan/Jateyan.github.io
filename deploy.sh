b666956a9dd61e1945d1d0b0cc3c2594f724a6a7
yanxp <yanxp@ti-net.com.cn>
2024-10-28 17:08:50 +0800

    初始化脚本

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
  
-# 确保脚本抛出遇到的错误
+# ȷ���ű��׳������Ĵ���
 set -e
  
-# 生成静态文件 , yarn docs:build
+# ���ɾ�̬�ļ� , yarn docs:build
 npm run docs:build
 rm -rf ../blog/dist/*
 
-# 将build生成的dist目录拷贝至上一层目录中
+# ��build���ɵ�distĿ¼��������һ��Ŀ¼��
 cp -rf dist ../blog/
 
-# 进入生成的文件夹
+# �������ɵ��ļ���
 cd ../blog/dist
 
-# git初始化，每次初始化不影响推送
+# git��ʼ����ÿ�γ�ʼ����Ӱ������
 git init
 git add -A
 git commit -m 'deploy'
 git branch -M main
 
-# 如果你想要部署到 https://USERNAME.github.io
+# �������Ҫ���� https://USERNAME.github.io
 git push -f git@github.com:Jateyan/jatej.github.io.git main
