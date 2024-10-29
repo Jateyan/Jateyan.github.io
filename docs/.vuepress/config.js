976d98cd414a61e59bf1b3013de1f37e9d16ca7e
yanxp <yanxp@ti-net.com.cn>
2024-10-29 17:51:50 +0800

    feat: 工单导入导出整体优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-  base:'./',
+  base:'/Jateyan/',
   title: 'VuePress',
   description: 'My first VuePress Site',
 
