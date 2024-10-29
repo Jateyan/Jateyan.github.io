74b4f5c561fbb35d4b988cdf4b19f097d2d6ecd1
yanxp <yanxp@ti-net.com.cn>
2024-10-29 11:45:23 +0800

    噢诶之

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,110 +1,141 @@
-const themeConfig = require('./config/theme/index.js')
-const navConf = require('./config/nav')
-const sidebarConf = require('./config/sidebar')
-const pluginsConf = require('./config/plugins/index')
-module.exports = {
-    //注意，此处需要填写你部署在nginx下的文件夹名称，如果是根目录，那么可以注释掉此行，注释掉后本地打开index.html无法访问
-    //base: "/dist/",
-    title: "君哥聊编程",
-    description: '点赞、转发、收藏',
-    dest: './dist',
-    port: '7777',
-    head: [
-        ['link', { rel: 'icon', href: '/img/favicon.ico' }],
-		['meta', { name: 'keywords', content: '君哥聊编程,vuepress,自建博客,君哥' }],
-		['meta', { name: 'description', content: '专属于自学者的在线学习平台,这里有编程领域最完善最「体系化的」Java学习视频、如果你是小白快加入我们一起学最全最「体系化的」java知识吧，官方交流QQ群：827553720' }],
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ["meta", {name: "robots", content: "all"}],
-        ["meta", {name: "author", content: "君哥"}],
-		["link", { rel: "stylesheet", href: "/css/style.css" }],//显示nav小logo
-		["script", { charset: "utf-8", src: "/js/custom.js" }],//加载右侧菜单栏图片
-        // 百度统计
-        /*
-        ['script', {}, `
-            var _hmt = _hmt || [];
-            (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?e312f85a409131e18146064e62b19798";
-                var s = document.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(hm, s);
-            })();
-        `],
-        */
+import { blogPlugin } from '@vuepress/plugin-blog'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: [
+      '/',
+      {
+        text: 'Article',
+        link: '/article/',
+      },
+      {
+        text: 'Category',
+        link: '/category/',
+      },
+      {
+        text: 'Tag',
+        link: '/tag/',
+      },
+      {
+        text: 'Timeline',
+        link: '/timeline/',
+      },
     ],
-    theme: 'reco',
-    themeConfig: {
-        type: 'blog',
-        smoothScroll: true,
-        // 博客设置
-		/*
-        blogConfig: {
-            category: {
-                location: 2, // 在导航栏菜单中所占的位置，默认2
-                text: '分类' // 默认 “分类”
-            },
-            tag: {
-                location: 3, // 在导航栏菜单中所占的位置，默认3
-                text: '标签' // 默认 “标签”
-            }
-        },*/
-        /*
-        valineConfig: {
-            // your appId
-            appId: 'AwhnPnTxxzGzoHsz',
-            // your appKey
-            appKey: '0M6f1PazzzalVIukU',
-            recordIP:true,
-            placeholder:'来都来了，冒个泡再走呗...',
-            visitor:true,
+  }),
+
+  plugins: [
+    blogPlugin({
+      // Only files under posts are articles
+      filter: ({ filePathRelative }) =>
+        filePathRelative ? filePathRelative.startsWith('posts/') : false,
+
+      // Getting article info
+      getInfo: ({ frontmatter, title, data }) => ({
+        title,
+        author: frontmatter.author || '',
+        date: frontmatter.date || null,
+        category: frontmatter.category || [],
+        tag: frontmatter.tag || [],
+        excerpt:
+          // Support manually set excerpt through frontmatter
+          typeof frontmatter.excerpt === 'string'
+            ? frontmatter.excerpt
+            : data?.excerpt || '',
+      }),
+
+      // Generate excerpt for all pages excerpt those users choose to disable
+      excerptFilter: ({ frontmatter }) =>
+        !frontmatter.home &&
+        frontmatter.excerpt !== false &&
+        typeof frontmatter.excerpt !== 'string',
+
+      category: [
+        {
+          key: 'category',
+          getter: (page) => page.frontmatter.category || [],
+          layout: 'Category',
+          itemLayout: 'Category',
+          frontmatter: () => ({
+            title: 'Categories',
+            sidebar: false,
+          }),
+          itemFrontmatter: (name) => ({
+            title: `Category ${name}`,
+            sidebar: false,
+          }),
         },
-        */
-        authorAvatar: '/avatar.png',
-        // 最后更新时间
-        lastUpdated: '2021-04-07', // string | boolean
-        //repo: 'it235',
-        // 如果你的文档不在仓库的根部
-        //docsDir: 'docs',
-        // 可选，默认为 master
-        //docsBranch: 'source',
-        //editLinks: true,
-        //editLinkText: '在 GitHub 上编辑此页！',
-        // 作者
-        author: '君哥',
-        // 项目开始时间
-        startYear: '2022',
-        nav: navConf,
-        sidebar: sidebarConf,
-		sidebarDepth: 2,
-        // 自动形成侧边导航
-		sidebar: 'auto',
-        // logo: '/head.png',
-        // 搜索设置
-        search: true,
-        searchMaxSuggestions: 10,
-        // ICP备案
-        record: '沪ICP备xxxxx号-6',
-        recordLink: 'https://beian.miit.gov.cn/',
-		// 公网安备备案
-        cyberSecurityRecord: '沪公网安备 xxxxxxx号',
-        cyberSecurityLink: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=xxx05177',
-		//友链
-        friendLink: [
-            {
-                title: '君哥聊编程',
-                desc: 'Enjoy when you can, and endure when you must.',
-                email: 'xxx@qq.com',
-                link: 'https://www.it235.com'
-            },
-            {
-                title: '君哥的课堂',
-                desc: '君哥的课堂',
-                avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-                link: 'https://how.ke.qq.com/'
-            },
-        ]
-    },
-    markdown: {
-        lineNumbers: true
-    },
-    plugins: pluginsConf
-}
+        {
+          key: 'tag',
+          getter: (page) => page.frontmatter.tag || [],
+          layout: 'Tag',
+          itemLayout: 'Tag',
+          frontmatter: () => ({
+            title: 'Tags',
+            sidebar: false,
+          }),
+          itemFrontmatter: (name) => ({
+            title: `Tag ${name}`,
+            sidebar: false,
+          }),
+        },
+      ],
+
+      type: [
+        {
+          key: 'article',
+          // Remove archive articles
+          filter: (page) => !page.frontmatter.archive,
+          layout: 'Article',
+          frontmatter: () => ({
+            title: 'Articles',
+            sidebar: false,
+          }),
+          // Sort pages with time and sticky
+          sorter: (pageA, pageB) => {
+            if (pageA.frontmatter.sticky && pageB.frontmatter.sticky)
+              return pageB.frontmatter.sticky - pageA.frontmatter.sticky
+
+            if (pageA.frontmatter.sticky && !pageB.frontmatter.sticky) return -1
+
+            if (!pageA.frontmatter.sticky && pageB.frontmatter.sticky) return 1
+
+            if (!pageB.frontmatter.date) return 1
+            if (!pageA.frontmatter.date) return -1
+
+            return (
+              new Date(pageB.frontmatter.date).getTime() -
+              new Date(pageA.frontmatter.date).getTime()
+            )
+          },
+        },
+        {
+          key: 'timeline',
+          // Only article with date should be added to timeline
+          filter: (page) => page.frontmatter.date instanceof Date,
+          // Sort pages with time
+          sorter: (pageA, pageB) =>
+            new Date(pageB.frontmatter.date).getTime() -
+            new Date(pageA.frontmatter.date).getTime(),
+          layout: 'Timeline',
+          frontmatter: () => ({
+            title: 'Timeline',
+            sidebar: false,
+          }),
+        },
+      ],
+      hotReload: true,
+    }),
+  ],
+
+  bundler: viteBundler(),
+})
--- docs/README.md
+++ docs/README.md
@@ -1,28 +1,33 @@
 ---
 home: true
-heroText: '粉丝的个人博客'
-bgImage: '/img/5.jpg'
-bgImageStyle: {
-  height: '350px'
-}
-# faceImage: '/head.png'
-# heroImage: '/hero_white.png'
-# heroImageStyle: {
-#   maxHeight: '150px',
-#   display: block,
-#   margin: '6rem auto 1.5rem',
-#   borderRadius: '5%',
-#   boxShadow: '0 5px 18px rgba(0,0,0,0.5)'
-# }
-isShowTitleInHome: true
-actionText: About Me
-actionLink: /views/personalInfo/
+title: Home
+heroImage: https://vuejs.press/images/hero.png
+actions:
+  - text: Get Started
+    link: /getting-started.html
+    type: primary
+
+  - text: Introduction
+    link: https://vuejs.press/guide/introduction.html
+    type: secondary
+
 features:
-- title: Who am I？
-  details: A programmer? A human being? Or a matter combination?
-- title: Where am I？
-  details: In a city? On earth? Or in a small part of space?
-- title: What am I gona do？
-  details: Work? Struggle? Or go to a destination I don't know?
-# footer: 'reco_luan'
+  - title: Simplicity First
+    details: Minimal setup with markdown-centered project structure helps you focus on writing.
+  - title: Vue-Powered
+    details: Enjoy the dev experience of Vue, use Vue components in markdown, and develop custom themes with Vue.
+  - title: Performant
+    details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+  - title: Themes
+    details: Providing a default theme out of the box. You can also choose a community theme or create your own one.
+  - title: Plugins
+    details: Flexible plugin API, allowing plugins to provide lots of plug-and-play features for your site.
+  - title: Bundlers
+    details: Default bundler is Vite, while Webpack is also supported. Choose the one you like!
+
+footer: MIT Licensed | Copyright © 2018-present VuePress Community
 ---
+
+This is the content of home page. Check [Home Page Docs][default-theme-home] for more details.
+
+[default-theme-home]: https://vuejs.press/reference/default-theme/frontmatter.html#home-page
